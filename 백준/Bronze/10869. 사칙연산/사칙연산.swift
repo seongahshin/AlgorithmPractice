b242102c88a07be79bde5c655324dfce1fa5d918
Shin Seong Ah <90595710+seongahshin@users.noreply.github.com>
2024-09-20 20:57:27 +0900

    [Bronze V] Title: 사칙연산, Time: 4 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.swift
@@ -0,0 +1,11 @@
+if let input = readLine() {
+    let firstInput = input.split(separator: " ").compactMap{ Int($0) }
+    let A = firstInput[0]
+    let B = firstInput[1]
+    
+    print("\(A+B)")
+    print("\(A-B)")
+    print("\(A*B)")
+    print("\(A/B)")
+    print("\(A%B)")
+}
